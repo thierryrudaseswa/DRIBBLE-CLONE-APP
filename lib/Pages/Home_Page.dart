96f879b0bb0c9fe3b69fe571e626877390b93e6f
thierryrudaseswa <trudaseswa@gmail.com>
2024-02-10 16:05:38 +0200

    The dribble clone app for fighting against depression

---
+++ lib/Pages/Home_Page.dart
@@ -0,0 +1,207 @@
+import 'package:dribble/main.dart';
+import 'package:flutter/material.dart';
+import 'package:dribble/util/emoticon_face.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue[700],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(children: [
+
+            //greating 
+            Container(
+              decoration: BoxDecoration(
+               
+          
+              ),
+         
+              
+
+              child: Row(
+               mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  //hiii
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start, 
+                  children: [
+                    Text(
+                      "Hii Thierry",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      '28 Jan 2024',
+                      style: TextStyle(
+                        color: Colors.blue[200],
+                      ),
+                    ),
+                  ],
+                ),
+
+              
+                  //notification
+              
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue[600],
+                      borderRadius: BorderRadius.circular(12)
+                    ),
+                    padding: EdgeInsets.all(12),
+                    child: Icon(
+                                  Icons.notifications,
+                                  color: Colors.white,
+                    ),
+                  )
+              
+              
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 8,
+            ),
+
+            //search  
+
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.blue[600],
+                borderRadius: BorderRadius.circular(12)
+              ),
+              padding: EdgeInsets.all(12),
+              child: Row(
+               children: [
+                Icon(
+                  Icons.search,
+                  color: Colors.white,
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'Search',
+                    style: TextStyle(
+                      color: Colors.white
+                    ),
+                  
+                  )
+               ],
+              ),
+
+            ),
+         SizedBox(
+          height: 20,
+         ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                'How  a u feeling bruh ?',
+                style: TextStyle(
+                  color: Colors.white
+                ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    color: Colors.blue[200],
+                          borderRadius:BorderRadius.circular(12)
+                  ),
+                  padding: EdgeInsets.all(3),
+                  child: Icon(
+                    Icons.more_horiz,
+                    color: Colors.white
+                  ),
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+           
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children:[
+                Column(
+                  children: [
+                    EmotionFace(emoticonFace: '😥'),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'Bad',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),)
+                  ],
+                ),
+                Column(
+                  children: [
+                    EmotionFace(emoticonFace: '😊'),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'fine',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),)
+                  ],
+                ),
+                 Column(
+                  children: [
+                    EmotionFace(emoticonFace: '😂'),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'well',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),)
+                  ],
+                ),
+                 Column(
+                  children: [
+                    EmotionFace(emoticonFace: '🤣'),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text(
+                      'Excellent',
+                      style: TextStyle(
+                        color: Colors.white
+                      ),)
+                  ],
+                ),
+               
+              ],)
+             
+        
+         
+
+          ]),
+
+
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/util/emoticon_face.dart
@@ -0,0 +1,28 @@
+import 'package:dribble/Pages/Home_Page.dart';
+import 'package:flutter/material.dart';
+
+class EmotionFace extends StatelessWidget {
+
+  final String emoticonFace;
+  const EmotionFace({super.key,required this.emoticonFace});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.blue[600],
+        borderRadius: BorderRadius.circular(12)
+      ),
+      padding: EdgeInsets.all(12),
+
+      child: Center(
+        child: Text(
+        emoticonFace,
+        style: TextStyle(
+          fontSize: 28,
+        ),
+        ),
+      ),
+    );
+  }
+}
